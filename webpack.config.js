0a49aeebc8781cc26658c5dd42313a7c58d0677f
torreseam <615694479+torreseam@users_noreply.github.com>
2020-07-23 18:40:11 -0500

    optimizationg

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,50 @@ const webpack = require("webpack");
 
 const path = require("path");
 
-// require("jquery");
-// require("bootstrap");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 
 
 module.exports = { 
-    entry: './assets/js/script.js',
+    entry: {
+        app: "./assets/js/script.js",
+        events: './assets/js/events.js',
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        // path: path.resolve(__dirname + '/dist'),
+        path: __dirname + "/dist",
+        filename: "[name].bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpeg$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name(file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function (url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    }
+                ],
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", //the report output to ab HTML file in the dist folder
+        })
     ],
     mode: 'development'
 }
