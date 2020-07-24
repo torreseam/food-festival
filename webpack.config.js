d86fc4fd5e4d8d7ceb753ae3ea3d88ad8ea932f9
torreseam <615694479+torreseam@users_noreply.github.com>
2020-07-24 00:17:44 -0500

    intercept fetch request

--- webpack.config.js
+++ webpack.config.js
@@ -6,35 +6,37 @@ const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPl
 
 
 
-module.exports = { 
+module.exports = {
     entry: {
         app: "./assets/js/script.js",
-        events: './assets/js/events.js',
+        events: "./assets/js/events.js",
         schedule: "./assets/js/schedule.js",
         tickets: "./assets/js/tickets.js"
     },
     output: {
-        // path: path.resolve(__dirname + '/dist'),
-        path: __dirname + "/dist",
         filename: "[name].bundle.js",
+        path: __dirname + "/dist"
     },
     module: {
         rules: [
             {
-                test: /\.jpeg$/i,
+                test: /\.jpg$/,
                 use: [
                     {
-                        loader: "file-loader",
+                        loader: 'file-loader',
                         options: {
                             name(file) {
-                                return "[path][name].[ext]"
+                                return '[path][name].[ext]';
                             },
                             publicPath: function (url) {
-                                return url.replace("../", "/assets/")
+                                return url.replace('../', '/assets/');
                             }
                         }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
                     }
-                ],
+                ]
             }
         ]
     },
